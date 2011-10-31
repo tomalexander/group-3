a194c8102884d18fe0da26652d8a342237640372
Tom Alexander <craftkiller@gmail.com>
2011-10-31 19:14:45 -0400

    panda grid

--- terrain.py
+++ terrain.py
@@ -1,4 +1,5 @@
 import w_loader
+from direct.actor.Actor import Actor #for animated models
 
 class terrain:
     def __init__(self, _x, _y, _type, _w_loader):
@@ -6,9 +7,10 @@ class terrain:
         self.y = _y
         self.type = _type
         self.w_loader = _w_loader;
+        self.load_model()
 
     def load_model(self):
         self.model = Actor("models/panda-model", {"walk":"panda-walk4"})
-        self.model.reparentTo(self.w_loader)
+        self.model.reparentTo(render)
         self.model.setScale(0.005)
         self.model.setPos(self.x, self.y, 0)
--- w_loader.py
+++ w_loader.py
@@ -2,15 +2,22 @@ from terrain import terrain
 
 class w_loader:
     def __init__(self):
-        pass
+        self.cell_height = 50
+        self.cell_width = 50
+        self.cell_list = []
 
     def load_world(self, world_number):
         file_name = "worlds/world" + str(world_number) + ".txt"
         file_handle = open(file_name, 'r')
+        y_pos = 0
         for full_line in file_handle:
             line = full_line.strip()
-            self.handle_line(line)
+            self.handle_line(line, y_pos)
+            y_pos = y_pos + self.cell_height
 
-    def handle_line(self, line):
+    def handle_line(self, line, y_pos):
+        x_pos = 0
         for cell in line:
-            pass
+            new_cell = terrain(x_pos, y_pos, 0, self)
+            self.cell_list.append(new_cell)
+            x_pos = x_pos + self.cell_width
