a7f408491ae3b6d7903b6063a9fdcd0a86cc7c53
Tom Alexander <craftkiller@gmail.com>
2011-10-31 18:34:21 -0400

    file handling and such

---
+++ load_tester.py
@@ -0,0 +1,4 @@
+from loader import loader
+
+x = loader()
+x.load_world(1)
---
+++ loader.py
@@ -0,0 +1,14 @@
+class loader:
+    def __init__(self):
+        pass
+
+    def load_world(self, world_number):
+        file_name = "worlds/world" + str(world_number) + ".txt"
+        file_handle = open(file_name, 'r')
+        for full_line in file_handle:
+            line = full_line.strip()
+            self.handle_line(line)
+
+    def handle_line(self, line):
+        for cell in line:
+            pass
