6444e214e8494c53adcbb95be5f9636b2553ce04
Brett Kaplan <darklink259@gmail.com>
2011-11-02 17:25:27 -0400

    input might be done...

--- carData.py
+++ carData.py
@@ -3,13 +3,25 @@ from pandac.PandaModules import *#basic Panda modules
 from direct.interval.IntervalGlobal import *#for compound intervals
 from direct.task import Task#for update fuctions
 import math
+from car import Car
 
-class CarData():
+class CarData(DirectObject):
     """Holds all the cars. All of them."""
     def __init__ (self, spos, index):#takes in a list of x,y tuples, there should be 4 of these
         self.spos = spos
         self.index = index
         self.carlist = []
+        
+        self.accept("arrow_left", self.setKey, [0,True])
+        self.accept("arrow_right", self.setKey, [1,True])
+        self.accept("arrow_up", self.setKey, [2,True])
+        self.accept("arrow_down", self.setKey, [3,True])
+        self.accept("space", self.setKey, [4,True])
+        self.accept("arrow_left-up", self.setKey, [0,False])
+        self.accept("arrow_right-up", self.setKey, [1,False])
+        self.accept("arrow_up-up", self.setKey, [2,False])
+        self.accept("arrow_down-up", self.setKey, [3,False])
+        self.accept("space-up", self.setKey, [4,False])
         
         taskMgr.add(self.move, "outtaThaWayImDrivingHere")
         self.prevtime = 0
@@ -21,6 +33,10 @@ class CarData():
         newcar = Car(pos[0][0], pos[0][1], (math.degrees(math.atan2(15-pos[0][0], 15-pos[0][1]))-90)%360)
         self.carlist.append(newcar)
         return newcar
+        
+    def setKey(self, ind, value):
+        if self.carlist[self.index] != nil:
+            self.carlist[self.index].input[ind] = value
         
     def move(self, task):
         elapsed = task.time - self.prevtime
