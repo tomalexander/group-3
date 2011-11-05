6bfe6d033d83c0b78d2aa1256101fc8b82bc38bc
Brett Kaplan <darklink259@gmail.com>
2011-11-05 18:04:52 -0400

    stuff

--- car.py
+++ car.py
@@ -3,10 +3,14 @@ from velocity import Velocity
 from pandac.PandaModules import *#basic Panda modules
 from direct.showbase.DirectObject import DirectObject#for event handling
 from direct.actor.Actor import Actor#for animated models
+import sys, os
 
 class Car():
     """This is a car."""
     def __init__ (self, x=0, y=0, h=0):
+        #mydir = os.path.abspath(sys.path[0])
+        #mydir = Filename.fromOsSpecific(mydir).getFullpath()
+        #self.model = loader.loadModel(mydir + "/models/newcar.egg")
         self.model = Actor("models/panda-model")
         self.model.reparentTo(render)
         self.model.setScale(.005)
