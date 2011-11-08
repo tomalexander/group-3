a7642a9d8790ec30038d19a53bafb8e07a12cca0
Tom Alexander <craftkiller@gmail.com>
2011-11-07 23:51:11 -0500

    wrote browser.php

---
+++ browser.php
@@ -0,0 +1,53 @@
+<?
+
+$time = time();
+$ip = $_SERVER['REMOTE_ADDR'];
+$action = $_REQUEST["action"];
+$SERVER_TIMEOUT = 20;
+
+if (!$action)
+    die();
+
+$db = sqlite_open('hosts.db', 0666, $error);
+if (!$db) die ($error);
+
+$query = "CREATE TABLE hosts(" . 
+       "name text, time int)";
+$ok = @sqlite_exec($db, $query, $error);
+
+if (!$ok && $error != "table hosts already exists")
+   die("Cannot execute query. $error");
+
+if ($action == "ping")
+{
+    $query = "INSERT INTO hosts VALUES(\"" . $ip . "\", " . $time . ")";
+    $ok = sqlite_exec($db, $query, $error);
+    if (!$ok) die("Cannot insert new time. " . $error);
+}
+
+if ($action == "gethosts")
+{
+    $query = "SELECT name, time FROM hosts ORDER BY time DESC LIMIT 10";
+    $result = sqlite_query($db, $query);
+    $hosts = array();
+    if ($result)
+    {
+        while ($row = sqlite_fetch_array($result, SQLITE_ASSOC))
+        {
+            if (!in_array($row["name"], $hosts) && $time - $row["time"] < $SERVER_TIMEOUT)
+            {
+                echo $row["name"] . " ";
+                $hosts[] = $row["name"];
+            }
+        }
+    }
+
+}
+
+$query = "DELETE FROM hosts WHERE time < " . $time-20;
+$ok = sqlite_exec($db, $query, $error);
+if (!$ok) die("Cannot delete expired times" . $error);
+
+sqlite_close($db);
+
+?>
--- main_menu.py
+++ main_menu.py
@@ -9,7 +9,7 @@ class panda_window(wx.Frame):
 
     def __init__(self, parent, title):
         super(panda_window, self).__init__(parent, title=title, 
-            size=(260, 180))
+            size=(300, 360))
         global panda_window_action
         panda_window_action = "none"
         self.init_ui()
@@ -19,12 +19,18 @@ class panda_window(wx.Frame):
     def init_ui(self):
         self.panel = wx.Panel(self, -1)
         self.label_ip_address = wx.StaticText(self.panel, label="Please Enter IP Address:", pos=(3,3))
-        self.text_ip_address = wx.TextCtrl(self.panel, pos=(3,33), size=(250, 50))
+        self.text_ip_address = wx.TextCtrl(self.panel, pos=(3,33), size=(290, 50))
         self.button_host = wx.Button(self.panel, 1, label="Host", pos=(3,90), size=(90, 28))
         self.button_connect = wx.Button(self.panel, 2, label="Connect", pos=(103,90), size=(90, 28))
+        self.button_refresh = wx.Button(self.panel, 3, label="Refresh", pos=(203,90), size=(90, 28))
         self.Bind(wx.EVT_BUTTON, self.click_host, id=1)
         self.Bind(wx.EVT_BUTTON, self.click_connect, id=2)
+        self.Bind(wx.EVT_BUTTON, self.click_refresh, id=3)
+        self.server_list = wx.ListBox(self.panel, id=4, pos=(3,140), size=(280,200))
+        wx.EVT_LISTBOX(self, 4, self.ip_selected)
+        self.hosts = []
         self.get_ip_address()
+        self.update_host_list()
         
     def get_ip_address(self):
         response = urllib2.urlopen('http://ip.paphus.com/')
@@ -47,6 +53,19 @@ class panda_window(wx.Frame):
         panda_window_action = "connect"
         self.start_game()
 
+    def click_refresh(self, event):
+        self.update_host_list()
+
+    def update_host_list(self):
+        self.hosts = []
+        response = urllib2.urlopen('http://ip.paphus.com/browser.php?action=gethosts')
+        ip = response.read()
+        self.hosts = [i for i in ip.split(" ") if i != ""]
+        self.server_list.Set(self.hosts)
+
+    def ip_selected(self, event):
+        self.text_ip_address.SetValue(self.hosts[self.server_list.GetSelection()])
+        
 def run_main_menu():
     app = wx.App()
     main_menu = panda_window(None, title='Main Selection')
