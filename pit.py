fa86443c55cce4719692a7c5a0edac57cf138946
Andrew Keohane <keohaa@rpi.edu>
2011-11-08 20:35:26 -0500

    Changed the collision sphere for pits.

--- pit.py
+++ pit.py
@@ -15,8 +15,8 @@ class pit(trap):
         self.set_up_collisions()
 
     def set_up_collisions(self):
-        self.collision_sphere = CollisionSphere((0,0,0), 6.5)
+        self.collision_sphere = CollisionSphere((0,0,5), 2)
         self.collision_node = CollisionNode("pit")
         self.collision_node.addSolid(self.collision_sphere)
         self.collision_node_path = self.model.attachNewNode(self.collision_node)
-        #self.collision_node_path.show()
+        self.collision_node_path.show()
