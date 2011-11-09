48b3ebce917963ea9c0b71347b08e2ef0a0b114d
Tom Alexander <craftkiller@gmail.com>
2011-11-08 23:32:47 -0500

    making more functionality

--- main_menu.py
+++ main_menu.py
@@ -35,6 +35,8 @@ class panda_window(wx.Frame):
         
         self.init_join_ui()
         self.init_host_ui()
+        self.get_ip_address()
+        
 
 
     def init_join_ui(self):
@@ -52,9 +54,17 @@ class panda_window(wx.Frame):
         self.join_hbox2 = wx.BoxSizer(wx.HORIZONTAL)
         self.label_name = wx.StaticText(self.join_panel, label="Name:")
         self.label_name.SetFont(wx.Font(14, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
-        self.text_ip_address = wx.TextCtrl(self.join_panel)
-        self.join_hbox2.Add(self.label_name, 1, wx.TOP | wx.LEFT | wx.EXPAND, 10)
-        self.join_hbox2.Add(self.text_ip_address, 3, wx.TOP | wx.RIGHT, 10)
+        self.text_name = wx.TextCtrl(self.join_panel)
+        self.join_hbox2.Add(self.label_name, 1, wx.TOP | wx.LEFT | wx.EXPAND, 0)
+        self.join_hbox2.Add(self.text_name, 3, wx.TOP | wx.RIGHT, 0)
+
+        #row 4
+        self.join_hbox4 = wx.BoxSizer(wx.HORIZONTAL)
+        self.label_ip = wx.StaticText(self.join_panel, label="IP Address:")
+        self.label_ip.SetFont(wx.Font(14, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+        self.text_ip = wx.TextCtrl(self.join_panel)
+        self.join_hbox4.Add(self.label_ip, 0, wx.TOP | wx.LEFT | wx.EXPAND, 0)
+        self.join_hbox4.Add(self.text_ip, 3, wx.TOP | wx.RIGHT, 0)
 
         #row 3
         #row 3 column 1
@@ -63,7 +73,9 @@ class panda_window(wx.Frame):
         self.label_running = wx.StaticText(self.join_panel, label="Running\nServers:")
         self.label_running.SetFont(wx.Font(14, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
         self.button_refresh = wx.Button(self.join_panel, 3, label="Refresh")
+        wx.EVT_BUTTON(self, 3, self._update_host_list)
         self.button_start = wx.Button(self.join_panel, 4, label="Start")
+        wx.EVT_BUTTON(self, 4, self.join_start_game)
         self.server_list = wx.ListBox(self.join_panel, id=4)
         self.join_vbox1.Add(self.label_running, 1, wx.ALL | wx.EXPAND, 5)
         self.join_vbox1.Add(self.button_refresh, 1, wx.ALL | wx.EXPAND, 5)
@@ -72,14 +84,16 @@ class panda_window(wx.Frame):
         self.join_hbox3.Add(self.server_list, 3, wx.TOP | wx.RIGHT | wx.EXPAND, 10)
 
         #rack em up
-        self.join_vbox.Add(self.join_hbox1, 1, wx.ALL | wx.EXPAND, 10)
-        self.join_vbox.Add(self.join_hbox2, 1, wx.ALL | wx.EXPAND, 10)
-        self.join_vbox.Add(self.join_hbox3, 2, wx.ALL | wx.EXPAND, 10)
+        self.join_vbox.Add(self.join_hbox1, 0, wx.ALL | wx.EXPAND, 10)
+        self.join_vbox.Add(self.join_hbox2, 0, wx.ALL | wx.EXPAND, 10)
+        self.join_vbox.Add(self.join_hbox4, 0, wx.ALL | wx.EXPAND, 10)
+        self.join_vbox.Add(self.join_hbox3, 1, wx.ALL | wx.EXPAND, 10)
 
         #Interactivtize me baby
         wx.EVT_BUTTON(self, 2, self._set_host_ui)
 
         self.join_panel.SetSizer(self.join_vbox)
+        self.update_host_list()
         #self.join_vbox.Add(self.join_panel)
         
 
@@ -90,12 +104,67 @@ class panda_window(wx.Frame):
         self.host_hbox1 = wx.BoxSizer(wx.HORIZONTAL)
         self.host_label_title = wx.StaticText(self.host_panel, label="Rezolution")
         self.host_label_title.SetFont(wx.Font(20, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD))
-        self.button_host = wx.Button(self.host_panel, 2, label="Join")
+        self.button_host = wx.Button(self.host_panel, 5, label="Join")
         self.host_hbox1.Add(self.host_label_title, 3, wx.TOP | wx.LEFT | wx.EXPAND, 10)
         self.host_hbox1.Add(self.button_host, 1, wx.TOP | wx.RIGHT, 10)
 
+        #row 2
+        self.host_hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        self.host_label_name = wx.StaticText(self.host_panel, label="Name:")
+        self.host_label_name.SetFont(wx.Font(14, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+        self.host_text_name = wx.TextCtrl(self.host_panel)
+        self.host_hbox2.Add(self.host_label_name, 1, wx.TOP | wx.LEFT | wx.EXPAND, 0)
+        self.host_hbox2.Add(self.host_text_name, 3, wx.TOP | wx.RIGHT, 0)
+
+        #row 4
+        self.host_hbox4 = wx.BoxSizer(wx.HORIZONTAL)
+        self.host_label_ip = wx.StaticText(self.host_panel, label="IP Address:")
+        self.host_label_ip.SetFont(wx.Font(14, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+        self.host_text_ip = wx.TextCtrl(self.host_panel)
+        self.host_hbox4.Add(self.host_label_ip, 0, wx.TOP | wx.LEFT | wx.EXPAND, 0)
+        self.host_hbox4.Add(self.host_text_ip, 3, wx.TOP | wx.RIGHT, 0)
+
+        #row 5
+        self.host_hbox5 = wx.BoxSizer(wx.HORIZONTAL)
+        self.host_label_world = wx.StaticText(self.host_panel, label="World:")
+        self.host_label_world.SetFont(wx.Font(14, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+        self.map_list = wx.ComboBox(self.host_panel, id=6, style=wx.CB_DROPDOWN)
+        self.host_hbox5.Add(self.host_label_world, 1, wx.TOP | wx.LEFT | wx.EXPAND, 0)
+        self.host_hbox5.Add(self.map_list, 3, wx.TOP | wx.RIGHT, 0)
+
+        #row 6
+        self.host_hbox6 = wx.BoxSizer(wx.HORIZONTAL)
+        self.host_label_time = wx.StaticText(self.host_panel, label="Time:")
+        self.host_label_time.SetFont(wx.Font(14, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+        self.host_text_time = wx.TextCtrl(self.host_panel)
+        self.host_text_time.SetValue("3")
+
+        self.host_label_players = wx.StaticText(self.host_panel, label="Players:")
+        self.host_label_players.SetFont(wx.Font(14, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+        self.host_players = wx.ComboBox(self.host_panel, id=7, style=wx.CB_DROPDOWN)
+        self.host_players.SetItems(["1","2","3","4"])
+        self.host_players.SetSelection(3)
+        
+        self.host_hbox6.Add(self.host_label_time, 0, wx.TOP | wx.LEFT | wx.EXPAND, 0)
+        self.host_hbox6.Add(self.host_text_time, 3, wx.TOP | wx.RIGHT, 03)
+        self.host_hbox6.Add(self.host_label_players, 0, wx.TOP | wx.RIGHT, 03)
+        self.host_hbox6.Add(self.host_players, 3, wx.TOP | wx.RIGHT, 0)
+
+        #row 7
+        self.host_hbox7 = wx.BoxSizer(wx.HORIZONTAL)
+        self.host_start_button = wx.Button(self.host_panel, 10, label="Start")
+        self.host_hbox7.Add(self.host_start_button, 0, wx.LEFT | wx.TOP, 0)
+
         #rack em up
         self.host_vbox.Add(self.host_hbox1, 1, wx.ALL | wx.EXPAND, 10)
+        self.host_vbox.Add(self.host_hbox2, 1, wx.ALL | wx.EXPAND, 10)
+        self.host_vbox.Add(self.host_hbox4, 1, wx.ALL | wx.EXPAND, 10)
+        self.host_vbox.Add(self.host_hbox5, 1, wx.ALL | wx.EXPAND, 10)
+        self.host_vbox.Add(self.host_hbox6, 1, wx.ALL | wx.EXPAND, 10)
+        self.host_vbox.Add(self.host_hbox7, 1, wx.ALL | wx.EXPAND, 10)
+
+        #Interactivtize me baby
+        wx.EVT_BUTTON(self, 5, self._set_join_ui)
 
         self.host_panel.SetSizer(self.host_vbox)
         
@@ -109,11 +178,64 @@ class panda_window(wx.Frame):
         self.host_panel.Hide()
         self.join_panel.Show()
         self.sizer.Layout()
+        self.text_name.SetValue(self.host_text_name.GetValue())
+        self.text_ip.SetValue(self.host_text_ip.GetValue())
+        self.update_host_list()
+        
 
     def set_host_ui(self):
         self.host_panel.Show()
         self.join_panel.Hide()
         self.sizer.Layout()
+        self.host_text_name.SetValue(self.text_name.GetValue())
+        self.get_ip_address()
+
+    def get_ip_address(self):
+        response = urllib2.urlopen('http://ip.paphus.com/')
+        ip = response.read()
+        self.text_ip.SetValue(ip)
+        self.host_text_ip.SetValue(ip)
+
+    def join_start_game(self, event):
+        self.start_game()
+
+    def start_game(self):
+        self.Close(True)
+        self.Destroy()
+
+    def click_host(self, event):
+        global panda_window_action
+        panda_window_action = "host"
+        self.start_game()
+
+    def click_connect(self, event):
+        global panda_window_action
+        global panda_window_ip_address
+        panda_window_ip_address = self.text_ip_address.GetValue()
+        panda_window_action = "connect"
+        self.start_game()
+
+    def click_refresh(self, event):
+        self.update_host_list()
+
+    def _update_host_list(self, event):
+        self.update_host_list()
+
+    def update_host_list(self):
+        self.hosts = []
+        response = urllib2.urlopen('http://ip.paphus.com/browser.php?action=gethosts')
+        ip = response.read()
+        self.hosts = [i for i in ip.split(" ") if i != ""]
+        self.server_list.Set(self.hosts)
+
+    def ip_selected(self, event):
+        self.text_ip_address.SetValue(self.hosts[self.server_list.GetSelection()])
+
+    def update_map_list(self):
+        self.maps = map(strip_txt, os.listdir("worlds/"))
+        self.map_list.SetItems(self.maps)
+        self.map_list.SetSelection(0)
+
 
     
 def strip_txt(inp):
