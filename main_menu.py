363e4f70c24199ec14a6a357f950db0a628d534e
Tom Alexander <craftkiller@gmail.com>
2011-11-08 19:03:31 -0500

    stuff

--- main_menu.py
+++ main_menu.py
@@ -1,6 +1,7 @@
 import wx
 import socket
 import urllib2
+import os
 
 panda_window_action = "none"
 panda_window_ip_address = ""
@@ -9,7 +10,7 @@ class panda_window(wx.Frame):
 
     def __init__(self, parent, title):
         super(panda_window, self).__init__(parent, title=title, 
-            size=(300, 360))
+            size=(600, 560))
         global panda_window_action
         panda_window_action = "none"
         self.init_ui()
@@ -18,20 +19,42 @@ class panda_window(wx.Frame):
 
     def init_ui(self):
         self.panel = wx.Panel(self, -1)
-        self.label_ip_address = wx.StaticText(self.panel, label="Please Enter IP Address:", pos=(3,3))
-        self.text_ip_address = wx.TextCtrl(self.panel, pos=(3,33), size=(290, 50))
-        self.button_host = wx.Button(self.panel, 1, label="Host", pos=(3,90), size=(90, 28))
-        self.button_connect = wx.Button(self.panel, 2, label="Connect", pos=(103,90), size=(90, 28))
-        self.button_refresh = wx.Button(self.panel, 3, label="Refresh", pos=(203,90), size=(90, 28))
-        self.Bind(wx.EVT_BUTTON, self.click_host, id=1)
-        self.Bind(wx.EVT_BUTTON, self.click_connect, id=2)
-        self.Bind(wx.EVT_BUTTON, self.click_refresh, id=3)
-        self.label_ip_address = wx.StaticText(self.panel, label="Running Servers:", pos=(3,140))
-        self.server_list = wx.ListBox(self.panel, id=4, pos=(3,160), size=(260,200))
-        wx.EVT_LISTBOX(self, 4, self.ip_selected)
+        self.set_join_gui()
+        
+        self.ln = wx.StaticLine(self.panel, -1, style=wx.LI_VERTICAL, pos=(300,0))
+        self.ln.SetSize((30,560))
+
+        self.set_host_gui()
+        
         self.hosts = []
+        self.maps = []
+        self.update_map_list()
         self.get_ip_address()
         self.update_host_list()
+        self.update_map_list()
+
+    def set_host_gui(self):
+        self.map_list = wx.ComboBox(self.panel, id=5, pos=(330,160), size=(150,50), style=wx.CB_DROPDOWN)
+        self.label_host = wx.StaticText(self.panel, label="Hosting", pos=(318,3))
+        self.label_host.SetFont(wx.Font(20, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD))
+        self.label_game_name = wx.StaticText(self.panel, label="Game Name:", pos=(318,23))
+        self.button_host = wx.Button(self.panel, 1, label="Host", pos=(312,140), size=(90, 28))
+        self.Bind(wx.EVT_BUTTON, self.click_host, id=1)
+        
+
+    def set_join_gui(self):
+        self.label_join = wx.StaticText(self.panel, label="Join", pos=(18,3))
+        self.label_join.SetFont(wx.Font(20, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD))
+        self.label_ip_address = wx.StaticText(self.panel, label="Please Enter IP Address:", pos=(12,60))
+        self.text_ip_address = wx.TextCtrl(self.panel, pos=(12,83), size=(290, 50))
+        self.button_connect = wx.Button(self.panel, 2, label="Connect", pos=(12,140), size=(100, 28))
+        self.button_refresh = wx.Button(self.panel, 3, label="Refresh", pos=(203,140), size=(90, 28))
+        self.Bind(wx.EVT_BUTTON, self.click_host, id=1)
+        self.Bind(wx.EVT_BUTTON, self.click_connect, id=2)
+        self.label_ip_address = wx.StaticText(self.panel, label="Running Servers:", pos=(12,180))
+        self.server_list = wx.ListBox(self.panel, id=4, pos=(12,210), size=(260,200))
+        wx.EVT_LISTBOX(self, 4, self.ip_selected)
+
         
     def get_ip_address(self):
         response = urllib2.urlopen('http://ip.paphus.com/')
@@ -66,6 +89,16 @@ class panda_window(wx.Frame):
 
     def ip_selected(self, event):
         self.text_ip_address.SetValue(self.hosts[self.server_list.GetSelection()])
+
+    def update_map_list(self):
+        self.maps = map(strip_txt, os.listdir("worlds/"))
+        self.map_list.SetItems(self.maps)
+        self.map_list.SetSelection(0)
+        
+
+def strip_txt(inp):
+    if (inp[-4:] == ".txt"):
+        return inp[:-4]
         
 def run_main_menu():
     app = wx.App()
